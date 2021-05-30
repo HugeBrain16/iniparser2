4c7e3c6d160fd2dd2b999543a603e09981127212
HugeBrain16 <joshtuck373@gmail.com>
2021-05-30 17:06:00 +0200

    compile patterns, fixing codes, `is_ini` removed

--- iniparser2/__init__.py
+++ iniparser2/__init__.py
@@ -2,8 +2,9 @@
 
 import re
 import io
+import ast
 
-__version__ = "2.3.0"
+__version__ = "2.4.0"
 __all__ = ["ParsingError", "INI", "PropertyError", "DuplicateError", "SectionError"]
 
 
@@ -44,7 +45,7 @@ class ParseSectionError(ParsingError):
     """raised when failed parsing section"""
 
 
-class INI(object):
+class INI:
     """main class for parsing ini"""
 
     def __init__(self, delimiter=("=",), convert_property=False):
@@ -52,6 +53,20 @@ class INI(object):
         self.delimiter = delimiter
         self.convert_property = convert_property
         self._sections = list()
+
+        # parser patterns
+        self._property_pattern = re.compile(
+            rf"^\s*(.+?)\s*[{r'|'.join(delimiter)}]\s*(.+?)?\s*$"
+        )
+        self._key_pattern = re.compile(r"^\s*(\#)|((.*)\s[#])")
+        self._val_pattern = re.compile(r"((.)^[#]$)|\s([#])")
+        self._section_pattern = re.compile(r"^\s*\[(.*)\]\s*?(.*)$")
+        self._comment_pattern = re.compile(r"^[#;]")
+
+        # converter patterns
+        self._float_pattern = re.compile(r"^[-+]?(\d*[.])\d*$")
+        self._int_pattern = re.compile(r"^[-+]?\d+$")
+        self._str_pattern = re.compile(r'".*?(?<!\\)(?:\\\\)*?"')
 
     def __enter__(self):
         return self
@@ -84,7 +99,7 @@ class INI(object):
             del self.ini[key]
 
     def read(self, string):
-        self.ini = parse(string, self.delimiter, self.convert_property)
+        self.ini = self._parse(string)
         self._sections = []
         for prop in self.ini:
             if isinstance(self.ini[prop], dict):
@@ -104,9 +119,7 @@ class INI(object):
 
     def read_file(self, filename):
         """read sections and properties"""
-        self.ini = parse(
-            open(filename, "r").read(), self.delimiter, self.convert_property
-        )
+        self.ini = self._parse(open(filename, "r").read())
         self._sections = []
         for prop in self.ini:
             if isinstance(self.ini[prop], dict):
@@ -125,24 +138,22 @@ class INI(object):
                 raise PropertyError("property %s not found" % name)
 
             del self.ini[name]
-            return None
+        else:
+            if not self.has_section(section):
+                raise SectionError("section %s not found" % section)
+            if not self.has_property(name, section):
+                raise PropertyError(f"property {name} not found in section {section}")
 
-        if not self.has_section(section):
-            raise SectionError("section %s not found" % section)
-        if not self.has_property(name, section):
-            raise PropertyError(f"property {name} not found in section {section}")
-
-        del self.ini[section][name]
+            del self.ini[section][name]
 
     def set(self, name, value="", section=None):
         if section is None:
             self.ini.update({name: value})
-            return None
+        else:
+            if not self.has_section(section):
+                raise SectionError("section %s not found" % section)
 
-        if not self.has_section(section):
-            raise SectionError("section %s not found" % section)
-
-        self.ini[section].update({name: value})
+            self.ini[section].update({name: value})
 
     def get(self, name, section=None):
         if section is None:
@@ -169,132 +180,179 @@ class INI(object):
         """write properties and sections to file"""
         dump(filename, self.ini)
 
+    def _check_comment(self, string):
+        """check comment"""
+        sec = self._comment_pattern.match(string)
+        if sec:
+            return True
+        return False
 
-def parse(string, delimiter, convert_property):
-    """parse ini string returns ini dictionary"""
-    result = {}
+    def _parse_property(self, string):
+        """parse property returns property key and property value"""
+        if self._check_comment(string):
+            return None
+        prop = self._property_pattern.findall(string)
+        if not prop:
+            return None
+        if len(prop[0]) < 2:
+            return None
+        key, val = prop[0][0], prop[0][1]
+        _key = self._key_pattern.match(key)
+        if _key:
+            return None
+        val = self._val_pattern.split(val)[0]
 
-    lines = io.StringIO(string).readlines()
+        return key, val
 
-    prev_section = None
-    prev_property = None
+    def _is_property(self, string):
+        """check property"""
+        if self._parse_property(string) is not None:
+            return True
+        return False
 
-    for lineno, line in enumerate(lines):
-        lineno += 1
+    def _parse_section(self, string):
+        """parse section returns section name"""
+        if self._check_comment(string):
+            return None
+        sec = self._section_pattern.findall(string)
+        if not sec:
+            return None
+        if sec[0][1] and not self._comment_pattern.match(sec[0][1].strip()):
+            return None
+        _sec = re.match(r"(.*)\s[#]", sec[0][0])
+        if not _sec:
+            return sec[0][0]
 
-        if not line.strip():
-            continue
+    def _is_section(self, string):
+        """check section"""
+        if self._parse_section(string) is not None:
+            return True
+        return False
 
-        if is_section(line.strip()):
-            prev_section = parse_section(line.strip())
+    def _parse(self, string):
+        """parse ini string returns ini dictionary"""
+        result = {}
 
-            if prev_section in result:
-                raise ParseDuplicateError(
-                    "section already exists", prev_section, lineno
-                )
+        lines = io.StringIO(string).readlines()
 
-            result.update({prev_section: {}})
+        prev_section = None
+        prev_property = None
 
-        elif is_property(line.strip(), delimiter):
-            key, val = parse_property(line.strip(), delimiter)
+        for lineno, line in enumerate(lines):
+            lineno += 1
 
-            prev_property = key
-
-            if prev_section:
-                if prev_property in result[prev_section]:
-                    raise ParseDuplicateError(
-                        "property already exists", prev_property, lineno
-                    )
-
-                result[prev_section].update({key: val})
-            else:
-                if prev_property in result:
-                    raise ParseDuplicateError(
-                        "property already exists", prev_property, lineno
-                    )
-
-                result.update({key: val})
-
-        else:  # allow value only property, the dict value set to True
-            if re.match(r"^\s", line):
-                if prev_section:
-                    if prev_property:
-                        result[prev_section][prev_property] = result[prev_section][
-                            prev_property
-                        ] + ("\n" + line.strip())
-                        continue
-                elif prev_section is None:
-                    if prev_property:
-                        result[prev_property] = result[prev_property] + (
-                            "\n" + line.strip()
-                        )
-                        continue
-
-            if prev_section:
-                if line.strip() in result[prev_section]:
-                    raise ParseDuplicateError(
-                        "property already exists", line.strip(), lineno
-                    )
-
-                result[prev_section].update({line.strip(): True})
-            else:
-                if line.strip() in result:
-                    raise ParseDuplicateError(
-                        "property already exists", line.strip(), lineno
-                    )
-
-                result.update({line.strip(): True})
-
-    if convert_property:
-        return _convert_property(result)
-
-    return result
-
-
-def _convert_property(INI_dict):
-    """converter"""
-    eval_codes = [
-        (r"^[-+]?(\d*[.])\d*$", float),
-        (r"^[-+]?\d+$", int),
-        (r"^\"(.*)\"$", eval),
-    ]
-
-    for sectf in INI_dict:
-        if isinstance(INI_dict[sectf], dict):
-            for prop in INI_dict[sectf]:
-                for eval_code in eval_codes:
-                    if type(INI_dict[sectf][prop]).__name__ != "str":
-                        continue
-
-                    if re.match(eval_code[0], INI_dict[sectf][prop]):
-                        INI_dict[sectf][prop] = eval_code[1](INI_dict[sectf][prop])
-                        break
-
-                if type(INI_dict[sectf][prop]).__name__ != "str":
-                    continue
-
-                if INI_dict[sectf][prop].lower() == "true":
-                    INI_dict[sectf][prop] = True
-                elif INI_dict[sectf][prop].lower() == "false":
-                    INI_dict[sectf][prop] = False
-        else:
-            for eval_code in eval_codes:
-                if type(INI_dict[sectf]).__name__ != "str":
-                    continue
-
-                if re.match(eval_code[0], INI_dict[sectf]):
-                    INI_dict[sectf] = eval_code[1](INI_dict[sectf])
-                    break
-
-            if type(INI_dict[sectf]).__name__ != "str":
+            if not line.strip():
                 continue
 
-            if INI_dict[sectf].lower() == "true":
-                INI_dict[sectf] = True
-            elif INI_dict[sectf].lower() == "false":
-                INI_dict[sectf] = False
+            if self._is_section(line.strip()):
+                prev_section = self._parse_section(line.strip())
 
-    return INI_dict
+                if prev_section in result:
+                    raise ParseDuplicateError(
+                        "section already exists", prev_section, lineno
+                    )
+
+                result.update({prev_section: {}})
+
+            elif self._is_property(line.strip()):
+                key, val = self._parse_property(line.strip())
+
+                prev_property = key
+
+                if prev_section:
+                    if prev_property in result[prev_section]:
+                        raise ParseDuplicateError(
+                            "property already exists", prev_property, lineno
+                        )
+
+                    result[prev_section].update({key: val})
+                else:
+                    if prev_property in result:
+                        raise ParseDuplicateError(
+                            "property already exists", prev_property, lineno
+                        )
+
+                    result.update({key: val})
+
+            else:  # allow value only property, the dict value set to True
+                if re.match(r"^\s", line):
+                    if prev_section:
+                        if prev_property:
+                            result[prev_section][prev_property] = result[prev_section][
+                                prev_property
+                            ] + ("\n" + line.strip())
+                            continue
+                    elif prev_section is None:
+                        if prev_property:
+                            result[prev_property] = result[prev_property] + (
+                                "\n" + line.strip()
+                            )
+                            continue
+
+                if prev_section:
+                    if line.strip() in result[prev_section]:
+                        raise ParseDuplicateError(
+                            "property already exists", line.strip(), lineno
+                        )
+
+                    result[prev_section].update({line.strip(): True})
+                else:
+                    if line.strip() in result:
+                        raise ParseDuplicateError(
+                            "property already exists", line.strip(), lineno
+                        )
+
+                    result.update({line.strip(): True})
+
+        if self.convert_property:
+            return self._convert_property(result)
+
+        return result
+
+    def _convert_property(self, ini_dict):
+        """converter"""
+        eval_codes = [
+            (self._float_pattern, float),
+            (self._int_pattern, int),
+            (self._str_pattern, ast.literal_eval),
+        ]
+
+        for sectf in ini_dict:
+            if isinstance(ini_dict[sectf], dict):
+                for prop in ini_dict[sectf]:
+                    for eval_code in eval_codes:
+                        if type(ini_dict[sectf][prop]).__name__ != "str":
+                            continue
+
+                        if eval_code[0].match(ini_dict[sectf][prop]):
+                            ini_dict[sectf][prop] = eval_code[1](ini_dict[sectf][prop])
+                            break
+
+                    if type(ini_dict[sectf][prop]).__name__ != "str":
+                        continue
+
+                    if ini_dict[sectf][prop].lower() == "true":
+                        ini_dict[sectf][prop] = True
+                    elif ini_dict[sectf][prop].lower() == "false":
+                        ini_dict[sectf][prop] = False
+            else:
+                for eval_code in eval_codes:
+                    if type(ini_dict[sectf]).__name__ != "str":
+                        continue
+
+                    if eval_code[0].match(ini_dict[sectf]):
+                        ini_dict[sectf] = eval_code[1](ini_dict[sectf])
+                        break
+
+                if type(ini_dict[sectf]).__name__ != "str":
+                    continue
+
+                if ini_dict[sectf].lower() == "true":
+                    ini_dict[sectf] = True
+                elif ini_dict[sectf].lower() == "false":
+                    ini_dict[sectf] = False
+
+        return ini_dict
 
 
 def dump(filename, ini_dict):
@@ -307,62 +365,3 @@ def dump(filename, ini_dict):
                     file.write(f"{prop} = {ini_dict[sect][prop]}\n")
             else:
                 file.write(f"{sect} = {ini_dict[sect]}\n")
-
-
-def parse_property(string, delimiter):
-    """parse property returns property key and property value"""
-    if check_comment(string):
-        return None
-    prop = re.findall(rf"^\s*(.+?)\s*[{r'|'.join(delimiter)}]\s*(.+?)?\s*$", string)
-    if not prop:
-        return None
-    if len(prop[0]) < 2:
-        return None
-    key, val = prop[0][0], prop[0][1]
-    _key = re.match(r"^\s*(\#)|((.*)\s[#])", key)
-    if _key:
-        return None
-    val = re.split(r"((.)^[#]$)|\s([#])", val)[0]
-
-    return key, val
-
-
-def parse_section(string):
-    """parse section returns section name"""
-    if check_comment(string):
-        return None
-    sec = re.findall(r"^\s*\[(.*)\]\s*?(.*)$", string)
-    if not sec:
-        return None
-    if sec[0][1] and not re.match(r"^[#;]", sec[0][1].strip()):
-        return None
-    _sec = re.match(r"(.*)\s[#]", sec[0][0])
-    if not _sec:
-        return sec[0][0]
-
-
-def check_comment(string):
-    """check comment"""
-    sec = re.match(r"^[#;]", string)
-    if sec:
-        return True
-    return False
-
-
-def is_property(string, delimiter):
-    """check property"""
-    if parse_property(string, delimiter) is not None:
-        return True
-    return False
-
-
-def is_section(string):
-    """check section"""
-    if parse_section(string) is not None:
-        return True
-    return False
-
-
-def is_ini(filename):
-    """check file extension"""
-    return filename.endswith(".ini")
