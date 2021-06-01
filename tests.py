0d1dbb118353f60f7859d3f66152330910b41efc
HugeBrain16 <joshtuck373@gmail.com>
2021-06-01 05:34:37 +0200

    update tests

--- tests.py
+++ tests.py
@@ -47,3 +47,52 @@ def test_conversion():
     assert type(parser["size"]) is float, "boo"
     assert type(parser["roach"]) is bool, "boo"
     assert type(parser["mushroom"]) is str, "boo"
+
+def test_get():
+    file = """
+    rat = 5
+    size = 9.0
+    roach = false
+    """
+
+    parser = iniparser2.INI()
+    parser.read(file)
+
+    assert type(parser.get_int('rat')) is int, "boo"
+    assert type(parser.get_float("size")) is float, "boo"
+    assert type(parser.get_bool("roach")) is bool, "boo"
+
+def test_get_bool():
+    file = """
+    imposter_sus = true
+    me = false
+    fard = 1
+    fart = 0
+    amogus = no
+    no_more_amogus = yes
+    lights = off
+    bobux_generator = on
+    """
+
+    parser = iniparser2.INI()
+    parser.read(file)
+
+    assert parser.get_bool("imposter_sus") is True, "boo"
+    assert parser.get_bool("me") is False, "boo"
+    assert parser.get_bool("fard") is True, "boo"
+    assert parser.get_bool("fart") is False, "boo"
+    assert parser.get_bool("amogus") is False, "boo"
+    assert parser.get_bool("no_more_amogus") is True, "boo"
+    assert parser.get_bool("lights") is False, "boo"
+    assert parser.get_bool("bobux_generator") is True, "boo"
+
+def test_update():
+    parser = iniparser2.INI()
+    parser.update({"house": {"cockroach": "infinity", "trash": 0}})
+
+    assert "house" in parser, "boo"
+    assert type(parser["house"]) is dict, "boo"
+    assert "cockroach" in parser["house"], "boo"
+    assert "trash" in parser["house"], "boo"
+    assert parser["house"]["cockroach"] == "infinity", "boo"
+    assert parser["house"]["trash"] == 0, "boo"
